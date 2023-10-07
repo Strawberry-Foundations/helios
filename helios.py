ef7912449522201165ae479645b93db4d4a80f51
matteodev8 <59450459+matteodev8@users.noreply.github.com>
2023-10-07 08:43:45 +0200

    Small fixes

--- helios.py
+++ helios.py
@@ -1,10 +1,13 @@
 # Copyright 2023 Strawberry Foundations (matteodev8)
 
-from flask import Flask, template_renderer
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return template_renderer('index.html')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
